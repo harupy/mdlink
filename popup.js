586d3b03c6de4951de09db63bdaa23baa6e9fa13
harupy <hkawamura0130@gmail.com>
2019-07-07 13:46:11 +0900

    add id parse feature

--- popup.js
+++ popup.js
@@ -1,16 +1,23 @@
-chrome.tabs.query({active:true, currentWindow: true}, tabs => {
+chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
   const title = tabs[0].title;
   const url = tabs[0].url;
-  const mdLink = `[${title}](${url})`
-  copy2clipboard(mdLink)
+  const id = parseId(url);
+  const mdLink = `[${title + id}](${url})`;
+  copy2clipboard(mdLink);
 });
 
 function copy2clipboard(text) {
-  const copyFrom = document.createElement("textarea");
+  const copyFrom = document.createElement('textarea');
   copyFrom.textContent = text;
   document.body.appendChild(copyFrom);
   copyFrom.select();
   document.execCommand('copy');
   copyFrom.blur();
   document.body.removeChild(copyFrom);
+}
+
+function parseId(url) {
+  const regexp = /.+#([^/]+)$/;
+  const match = regexp.exec(url);
+  return match ? ' : ' + match[1] : '';
 }
